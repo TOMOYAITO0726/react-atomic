e16288d06d54e6dfa2af41c9530b25fb45a623a4
Tomoya Ito <tomoyaito199507265724@gmail.com>
2023-12-07 10:15:26 +0900

    organismの作成完了

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisims/user/UserCard";
+import "./styles.css";
+
+const user = {
+  name: "いとと",
+  image: "https://source.unsplash.com//%E5%86%99%E7%9C%9F/%E6%9C%A8%E3%81%AE%E9%9A%A3%E3%81%AB%E7%AB%8B%E3%81%A3%E3%81%A6%E3%81%84%E3%82%8B%E5%B0%8F%E3%81%95%E3%81%AA%E7%99%BD%E3%81%84%E7%8A%AC-KnSYF3b4KcM",  
+  email: "1111@example.com",
+  phone:"000-000-000",
+  company: {
+    name:"テスト株式会社"
+  },
+   website:"https://qiita.com/"
+};
 
 export default function App() {
     return (
@@ -9,6 +22,7 @@ export default function App() {
             <SecondaryButton>検索</SecondaryButton>
             <br />
             <SearchInput />
+            <UserCard user={user}/>
         </div>
     );
 }
---
+++ src/components/organisims/user/UserCard.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { Card } from "../../atoms/card/Card";
+import { UserIconWithName } from "../../molecules/user/UserIconWithName";
+
+
+export const UserCard = (props) => {
+    const { user } = props;
+    return (
+        <Card>
+            <SDL>
+                <UserIconWithName image={user.image} name={user.name}/>
+                <dl>
+                <dt>メール</dt>
+                <dd>{user.emai}</dd>
+                <dt>TEL</dt>
+                <dd>{user.phone}</dd>
+                <dt>会社名</dt>
+                <dd>{user.company.name}</dd>
+                <dt>WEB</dt>
+                <dd>{user.website}</dd>
+                </dl>
+            </SDL>
+        </Card>
+    );
+};
+
+const SDL = styled.dl`
+    text-aligh: left;
+    margin-bottom: 0px;
+    dt {
+        float: left;
+    }
+    dd {
+        padding-left: 32px;
+        padding-bottom: 8px;
+        overflow-wrap: break-word;  //はみだした要素を折り返す
+    }
+`
+
+//overflow-wrap: break-word;  //はみだした要素を折り返す
